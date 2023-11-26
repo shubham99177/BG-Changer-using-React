8559584d115c95897a8e9811a31117f914e67cd8
shubham99177 <shubhamrathoree123@gmail.com>
2023-11-26 16:14:58 +0530

    correct

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import './App.css'
 
 function App() {
   const [color, setcolor] = useState("olive")
-  const [Radius, setradius] = useState("2px")
+  const [Radius] = useState("2px")
 
 
   return (
